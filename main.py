14cfd1b9e116d561be8d151cdcb8ea2249c464b9
acelebio <ali.celebiog@gmail.com>
2024-08-28 02:43:29 -0700

    added circleshape.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from constants import *
 def main():
     print("Starting asteroids!")
     pygame.init()
-
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-
+    dt = 0
     while True:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -19,6 +19,6 @@ def main():
         surface=pygame.Surface((SCREEN_WIDTH,SCREEN_HEIGHT))
         pygame.Surface.fill(surface,(255,255,0))
         pygame.display.flip()
-
+        dt = clock.tick(60)/1000
 if __name__ == "__main__":
         main()
