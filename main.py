5a082225805809b7256cb7b1e8e2c2fc7d3254f0
acelebio <ali.celebiog@gmail.com>
2024-08-29 00:17:18 -0700

    made moveable player

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main():
@@ -11,13 +12,24 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+
     dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                            return
-        surface=pygame.Surface((SCREEN_WIDTH,SCREEN_HEIGHT))
-        pygame.Surface.fill(surface,(255,255,0))
+            if event.type == pygame.QUIT:
+                return
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
 if __name__ == "__main__":
